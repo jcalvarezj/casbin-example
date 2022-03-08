e3e557fbe3ca0b3c15e862db8b0c1e62b18eb2b8
Juan Camilo Alvarez Jurado <camilo.alvarez@jikkosoft.com>
2022-03-08 18:24:37 -0500

    Implement draft of POC

---
+++ go.mod
@@ -0,0 +1,15 @@
+module main.go
+
+go 1.17
+
+require (
+	github.com/Blank-Xu/sql-adapter v0.0.0-20210714092925-0bdd0d548758
+	github.com/casbin/casbin/v2 v2.41.1
+	github.com/go-chi/chi v1.5.4
+	github.com/go-sql-driver/mysql v1.6.0
+)
+
+require (
+	github.com/Knetic/govaluate v3.0.1-0.20171022003610-9aa49832a739+incompatible // indirect
+	github.com/lib/pq v1.10.4 // indirect
+)
---
+++ main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+
+	sqladapter "github.com/Blank-Xu/sql-adapter"
+	"github.com/casbin/casbin/v2"
+	"github.com/go-chi/chi"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func finalizer(db *sql.DB) {
+	err := db.Close()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Authorizer(e *casbin.Enforcer) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		fn := func(w http.ResponseWriter, r *http.Request) {
+			role := r.Header.Get("Role")
+			resource := r.URL.RequestURI()
+			method := r.Method
+
+			if role == "" {
+				w.WriteHeader(http.StatusUnauthorized)
+				w.Write([]byte("ERROR - There is no role assigned\n"))
+				return
+			}
+
+			allowed, err := e.Enforce(role, resource, method)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write([]byte("ERROR - " + err.Error()))
+				return
+			}
+			if allowed {
+				next.ServeHTTP(w, r)
+			} else {
+				w.WriteHeader(http.StatusUnauthorized)
+				w.Write([]byte("ERROR - The current role (" + role + ") is not allowed to execute " + resource + " [" + method + "]\n"))
+				return
+			}
+		}
+
+		return http.HandlerFunc(fn)
+	}
+}
+
+func testPolicies(enforcer *casbin.Enforcer) {
+	// Policies mapping roles, resources, and methods
+	enforcer.AddPolicy("treasury", "/", "GET")
+	enforcer.AddPolicy("treasury", "/collection/*", "GET")
+	enforcer.AddPolicy("treasury", "/collection/*", "PUT")
+	enforcer.AddPolicy("treasury", "/collection/*", "PATCH")
+
+	enforcer.AddPolicy("lawyer", "/collection/*", "GET")
+
+	// Save policies in DB
+	if err := enforcer.SavePolicy(); err != nil {
+		log.Println("Could not save policies: ", err)
+	}
+}
+
+func main() {
+	dbName := "prueba"
+	dbUser := "prueba"
+	dbPass := "prueba"
+	dbPort := "3306"
+
+	connectionString := fmt.Sprintf("%s:%s@tcp(127.0.0.1:%s)/%s", dbUser, dbPass, dbPort, dbName)
+
+	db, err := sql.Open("mysql", connectionString)
+	if err != nil {
+		panic(err)
+	}
+	if err = db.Ping(); err != nil {
+		panic(err)
+	}
+
+	adapter, err := sqladapter.NewAdapter(db, "mysql", "casbin_rule_test")
+	if err != nil {
+		panic(err)
+	}
+
+	enforcer, err := casbin.NewEnforcer("config/basic_model.conf", adapter)
+	if err != nil {
+		panic(err)
+	}
+
+	if err = enforcer.LoadPolicy(); err != nil {
+		log.Println("Could not load policy: ", err)
+	}
+
+	port := "8080"
+
+	log.Printf("Starting up on http://localhost:%s", port)
+
+	r := chi.NewRouter()
+
+	testPolicies(enforcer)
+
+	r.Use(Authorizer(enforcer))
+
+	r.With(Authorizer(enforcer)).Get("/", func(w http.ResponseWriter, r *http.Request) {
+		role := r.Header.Get("Role")
+		w.Header().Set("Content-Type", "text/html")
+		w.Write([]byte("<h1>Hello World! - " + role + "</h1>\n"))
+	})
+
+	log.Fatal(http.ListenAndServe(":"+port, r))
+}
